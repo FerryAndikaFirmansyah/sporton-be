5b8d82935e80e56438ce4a144582dc9a4443b258
FerryAndikaFirmansyah <ferryandika089@gmail.com>
2026-02-01 18:39:28 +0700

    push all session 7

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import cors from "cors";
 import express from "express";
+import path from "path";
 import { authenticate } from "./middlewares/auth.middleware";
 import authRoutes from "./routes/auth.route";
+import categoryRoutes from "./routes/category.routes";
+import productRoutes from "./routes/product.route";
 
 const app = express();
+
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
 app.use("/api/auth", authRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("sporton backend API is running");
